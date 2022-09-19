35679bec40402ddc8f81719efd93c6abf0642c66
PramodSingh <pramodsinghneo@gmail.com>
2022-09-19 12:25:47 +0530

    main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:firebase_crud_demo/presentation/home_page.dart';
 import 'package:firebase_crud_demo/presentation/products_page/bloc/products_bloc.dart';
 import 'package:firebase_crud_demo/repository/product_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
