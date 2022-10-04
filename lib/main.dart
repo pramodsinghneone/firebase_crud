29891485a7b240c4d58b2f2a4464262c80587214
PramodSingh <pramodsinghneo@gmail.com>
2022-10-04 18:06:32 +0530

    main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget {
         ],
         child: GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Firebase Crud',
+          title: 'Firebase CRUD',
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
